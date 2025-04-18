27fef90f87749ad4bd3c070fdd9a492cdd24719a
PranavK-22 <112797777+PranavK-22@users.noreply.github.com>
2025-04-18 15:36:49 +0530

    Delete src/backend directory

--- src/backend/data/README.md
+++
@@ -1,1 +0,0 @@
-All the dataset files (from 20M dataset) go here, including your each trained epoch.
--- src/backend/utils.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-def cosine_similarity_matrix(matrix, query_vector):
-    dot_product = np.dot(matrix, query_vector)
-    norm_a = np.linalg.norm(matrix, axis=1)
-    norm_b = np.linalg.norm(query_vector)
-    return dot_product / (norm_a * norm_b + 1e-10)
